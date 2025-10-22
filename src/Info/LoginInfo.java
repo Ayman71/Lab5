16b298d5418b20b3e363b153f44f825aa10a9fcb
MariamMohammedSalah <157317328+MariamMohammedSalah@users.noreply.github.com>
2025-10-22 19:37:03 +0300

    first

---
+++ src/Info/LoginInfo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Info;
+
+/**
+ *
+ * @author Mariam
+ */
+public interface LoginInfo {
+ String Username= "Lab5";
+ String Password = "Programming";
+ 
+}
+    
+
